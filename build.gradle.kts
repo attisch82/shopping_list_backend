c895ff4cb8ba34ad77199e7f6a80041aa8b9d4ef
attisch82 <attila.farkas.sd@gmail.com>
2025-05-31 20:52:04 +0200

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.0"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.attila"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+
+	//Exposed
+	implementation("org.jetbrains.exposed:exposed-core:0.41.1")
+	implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
+	implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
+	implementation("org.jetbrains.exposed:exposed-java-time:0.41.1")
+	implementation("org.postgresql:postgresql:42.7.2")
+
+	//dotenv
+	implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
