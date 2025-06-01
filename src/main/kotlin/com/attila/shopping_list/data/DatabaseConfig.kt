f0aca9945eabcb471314593641bd374881368a09
attisch82 <attila.farkas.sd@gmail.com>
2025-06-01 17:45:14 +0200

    Creates Database config

--- src/main/kotlin/com/attila/shopping_list/data/DatabaseConfig.kt
+++ src/main/kotlin/com/attila/shopping_list/data/DatabaseConfig.kt
@@ -1,4 +1,22 @@
 package com.attila.shopping_list.data
 
+import io.github.cdimascio.dotenv.dotenv
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
+
+@Configuration
 class DatabaseConfig {
+    private val dotenv = dotenv()
+
+    @Bean
+    fun dataSource(): DataSource {
+        return DataSourceBuilder.create()
+            .url(dotenv["DB_URL"])
+            .username(dotenv["DB_USER"])
+            .password(dotenv["DB_PASSWORD"])
+            .driverClassName("org.postgresql.Driver")
+            .build()
+    }
 }
